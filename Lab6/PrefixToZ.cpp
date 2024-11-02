4851bbcaa4ba210249026a16b4a2565e2d98055e
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-11-02 18:39:36 +0200

    LastTask is 100% done for c++ and 76% for python

--- Lab6/PrefixToZ.cpp
+++ Lab6/PrefixToZ.cpp
@@ -4,41 +4,48 @@
 
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-
-    vector<int> P(n);
-
-    for (int i = 0; i < n; i++) {
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> P(N);
+    vector<int> Z(N);
+    for (int i = 0; i < N; i++)
+    {
         cin >> P[i];
     }
-
-    vector<int> Z(n);
-
-	for(int i = 1; i < n; i++)
+    for(int i = 1; i < N; i++)
+    {
         if(P[i])
-                Z[i - P[i] + 1] = P[i];
-    
+        {
+            Z[i - P[i] + 1] = P[i];
+        }
+    }
     Z[0] = 0;
         
     if(Z[1])
-            for(int i = 1; i < Z[1]; i++)
-                    Z[i + 1] = Z[1] - i;
-    
-    int t;
-    for(int i = Z[1] + 1; i < n - 1; i++)
     {
-            t = i;
-            if(Z[i] && !Z[i + 1])
-                    for(int j = 1; j < Z[i] && Z[i + j] <= Z[j]; j++)
-                    {
-                            Z[i + j] = min(Z[j], Z[i] - j);
-                            t = i + j;
-                    }
-            i = t;  
+        for(int i = 1; i < Z[1]; i++)
+        {
+            Z[i + 1] = Z[1] - i;
+        }
     }
-    for (int i = 0; i < n; i++)
+
+    int t;
+    for(int i = Z[1] + 1; i < N - 1; i++)
+    {
+        t = i;
+        if(Z[i] && !Z[i + 1])
+        {
+            for(int j = 1; j < Z[i] && Z[i + j] <= Z[j]; j++)
+            {
+                    Z[i + j] = min(Z[j], Z[i] - j);
+                    t = i + j;
+            }
+        }
+        i = t;  
+    }
+    for (int i = 0; i < N; i++)
     {
         cout << Z[i] << " ";
     }
